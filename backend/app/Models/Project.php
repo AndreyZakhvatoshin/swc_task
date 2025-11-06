deb984f74c9583853b9360ff59139a0bc02a8ca4
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-11-06 21:23:46 +0300

    feat: добавлены отношения

--- backend/app/Models/Project.php
+++ backend/app/Models/Project.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Project extends Model
 {
@@ -12,4 +14,21 @@ class Project extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function users(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            User::class,
+            Task::class,
+            'project_id',
+            'id',
+            'id',
+            'user_id'
+        )->distinct();
+    }
 }
