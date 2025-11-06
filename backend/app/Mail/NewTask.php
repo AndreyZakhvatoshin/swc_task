9f8a6d494e883f6885f98e58b63c08d5ec8d20f7
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-11-06 20:36:21 +0300

    feat: письмо о новой задаче

---
+++ backend/app/Mail/NewTask.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Task;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewTask extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public string $projectName,
+        public Task $task
+    ) {
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Task Created',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'view.name',
+            text: "В проекте $this->projectName новая задача {$this->task->title}"
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
