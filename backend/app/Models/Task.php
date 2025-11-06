1729be537f3065bc469b3900910e10b0c043ec7e
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-11-06 21:23:00 +0300

    feat: добавлены отношения

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Task extends Model
+class Task extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'completion_date',
+        'user_id',
+    ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
