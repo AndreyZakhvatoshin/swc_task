70e9265603ba5205e8977d61e0265c4256d77966
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-11-06 18:07:56 +0300

    feat: роуты регистрации и входа пользователя

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register'])->name('user.register');
+Route::post('/login', [AuthController::class, 'login'])->name('user.login');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
