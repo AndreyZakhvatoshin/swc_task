e8d24fbe1643393b89307c20066d7cc265569ae1
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-11-07 10:51:06 +0300

    feat: ресурс для задачи

---
+++ backend/app/Http/Resources/TaskResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Task
+ */
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $user = $this->user;
+        $mediaItems = $this->getMedia('attachments')
+            ->map(function ($item) {
+            return $item->getUrl();
+        });
+
+        return [
+            'id' => $this->id,
+            'user' => $user->name,
+            'title' => $this->title,
+            'status' => $this->status,
+            'completion_date' => $this->completion_date,
+            'created_at' => $this->created_at,
+            'attachments' => $mediaItems
+        ];
+    }
+}
