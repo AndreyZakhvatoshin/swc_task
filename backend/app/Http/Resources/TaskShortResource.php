6d04a3dfda02bb7be47d48faceb46d0d67fcd0dd
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-11-07 10:17:10 +0300

    feat: ресурс для списка задач

---
+++ backend/app/Http/Resources/TaskShortResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Task
+ */
+class TaskShortResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'status' => $this->status,
+            'completion_date' => $this->completion_date,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
