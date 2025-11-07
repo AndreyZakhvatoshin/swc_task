0f9be783846339322d4df2de2d96bf8a57f8aab0
Andrey Zakhvatoshin <mastetandron@gmail.com>
2025-11-07 11:52:35 +0300

    feat: роуты

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +12,11 @@ Route::post('/login', [AuthController::class, 'login'])->name('user.login');
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/projects/{project}/tasks', [ProjectController::class, 'getTasks'])->name('project.tasks.list');
+    Route::post('/projects/{project}/tasks', [ProjectController::class, 'addTask'])->name('project.tasks.add');
+    Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+});
